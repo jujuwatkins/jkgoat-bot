68280a7f13658073c0a06b6be6227d02049b2c59
jujuwatkins <jujuwatkins522@gmail.com>
2026-04-22 08:42:32 -0500

    Update messages in main.py with new content

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 CHANNEL_ID = 1478166066424451218
 
 MESSAGES = [
-    "I love Cryptic Vain",
-    "aew is better than wrestle btw",
-    "doors is the best ROBLOX horror game",
-    "Jakobe is not innocent he said those things"
-    "I hate Miggers"
+    "I love Crying Tip A. Vane",
+    "AEW is better than WRESTLE! and you aren't changing my mind",
+    "I hate Lip",
+    "Jakobe was born yesterday",
+    "MJF is the goat",
+    "I farted",
+    "Mulberry County is better than Doors",
+    "Doom Doom Doom Sahur",
+    "0 big booty latinas 😭😭😭😭",
+    "I nutted",
+    "fuck shit bitch damn cock sucker pussy asshole cunt",
+    "Elder loves penis",
 ]
 
 @bot.event
@@ -24,7 +31,9 @@ async def on_ready():
 @tasks.loop(hours=1)
 async def hourly_check():
     channel = bot.get_channel(CHANNEL_ID)
+
     if channel:
+        # 1/100 chance
         if random.randint(1, 100) == 1:
             await channel.send(random.choice(MESSAGES))
 
